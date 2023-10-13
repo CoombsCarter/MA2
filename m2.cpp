61806a0203d4f8580feffd805d203294231287fd
CoombsCarter <120283653+CoombsCarter@users.noreply.github.com>
2023-10-13 11:29:36 -0400

    Added getNum()
    added getNum() function from d2l along with a function comment

--- m2.cpp
+++ m2.cpp
@@ -19,3 +19,33 @@ main() {
 
 	return 0;
 }
+
+
+
+
+/*
+* Function: getNum()
+* Description: This function takes a user input and that input as an integer
+* Parameters: void
+* Returns: int number: the number the user inputs
+*/
+#pragma warning(disable: 4996)
+int getNum(void)
+{
+	/* we will see in a later lecture how we can improve this code */
+	char record[121] = { 0 }; /* record stores the string */
+	int number = 0;
+	/* NOTE to student: indent and brace this function consistent with
+	your others */
+	/* use fgets() to get a string from the keyboard */
+	fgets(record, 121, stdin);
+	/* extract the number from the string; sscanf() returns a number
+	* corresponding with the number of items it found in the string */
+	if (sscanf(record, "%d", &number) != 1)
+	{
+		/* if the user did not enter a number recognizable by
+		* the system, set number to -1 */
+		number = -1;
+	}
+	return number;
+}
