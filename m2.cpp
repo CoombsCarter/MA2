433d85011018ea3966fb32f28d50c313b441a1dd
CoombsCarter <120283653+CoombsCarter@users.noreply.github.com>
2023-10-13 11:25:50 -0400

    Added inital code
    File header comment, stdio.h, main()

---
+++ m2.cpp
@@ -0,0 +1,21 @@
+/*
+* FILE           : f4.cpp
+* PROJECT        : SENG1000 - Focused Assignment 4
+* PROGRAMMER     : Carter Coombs - 8868877
+* FIRST VERSION  : 2023-10-05
+* DESCRIPTION    :
+*	This program takes a users list of inputted integers and decides which integer is the lowest.
+*/
+
+
+
+
+#include <stdio.h>
+
+
+
+
+main() {
+
+	return 0;
+}
